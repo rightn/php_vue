828232573cff91788494ea94a114ae28481f3215
rightn <ygo9458@gmail.com>
2022-04-28 14:30:35 +0900

    준비중

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,23 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/create">Create</router-link> |          
+      <router-link to="/list">List</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+    
+    <router-view />
+  </div>
+  
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
---
+++ src/pages/DetailView.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="container">
+        <div class="card">
+            <div class="card-header">
+                Todo
+            </div>
+            <div class="card-body">
+                <h4 class="card-title">{{todo.title}}</h4>
+                <p class="card-text">{{todo.body}}</p>
+                <p class="card-text">{{todo.complete}}</p>
+                <p class="card-text">{{todo.body}}</p>
+                <div class="btn-group" role="group" aria-label="">
+                    <button type="button" class="btn btn-warning" @click="editTodo(todo.id)">수정</button>
+                    <button type="button" class="btn btn-danger" @click="deleteTodo(todo.id)">삭제</button>
+                    <button type="button" class="btn btn-primary">이전</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    import { reactive } from 'vue';
+    import { useRoute, useRouter } from 'vue-router'
+    export default {
+        setup() {
+            const route = useRoute();  
+            // 타이틀
+            const todo = reactive({
+                title: '',
+                body: '',
+                id: 0,
+                complete: 0
+            });
+            // console.log(route.params.id);
+            // 아이디를 전달하고 자료를 받아온다.
+            const getInfo = () => {
+                fetch(`http://rightn.dothome.co.kr/data_read_id.php?id=${route.params.id}`)
+                .then(res => res.json())
+                .then(data => {
+                    // console.log(data);
+                    todo.title = data.result[0].title;
+                    todo.body = data.result[0].body;
+                    todo.complete = data.result[0].complete;
+                    todo.id = data.result[0].id;
+                })
+                .catch()
+            }
+
+            getInfo();
+
+            // 할일 삭제 
+            const router = useRouter();
+            const deleteTodo = (_id) => {
+                // console.log(_id);
+                fetch(`http://rightn.dothome.co.kr/data_delete.php?id=${_id}`)
+                .then(res => res.json())
+                .then(data => {
+                    if(data.result == 1) {
+                        router.push('/list')
+                    }else{
+                        console.log('삭제에 실패했습니다');
+                    }        
+                })
+                .catch()
+            }
+                        
+            // 수정하기
+            const editTodo = (_id) => {
+                router.push({
+                    name: 'Update',
+                    params: { id: _id }
+                });
+            }
+
+            return {
+                todo,
+                deleteTodo,
+                editTodo
+            }
+        }
+    }
+</script>
+
+<style>
+
+</style>
